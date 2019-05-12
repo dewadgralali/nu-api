d1e9b1caf44a6cc6b94a3903a48f4810af45b790
Adhyaksa Jaya <adhyaksaj@gmail.com>
2019-05-12 20:23:39 +0700

    Added PostServiceContract

--- service/contracts.go
+++ service/contracts.go
@@ -11,3 +11,14 @@ type CategoryServiceContract interface {
 	Update(id uint, name string) error
 	Delete(id uint) error
 }
+
+// PostServiceContract represents interface for
+// post service
+type PostServiceContract interface {
+	Get() []model.Post
+	Create(title string, content string, categoriesID []uint) (model.Post, error)
+	Find(id uint) (model.Post, error)
+	FindBySlug(slug string) (model.Post, error)
+	Update(id uint, title string, content string, categoriesID []uint) (model.Post, error)
+	Delete(id uint) error
+}
